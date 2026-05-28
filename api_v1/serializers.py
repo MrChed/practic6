5a9dd885735be2ec4ba4e40dfee9e4b3ceb8a0c8
_MrChed_ <141160614+MrChed@users.noreply.github.com>
2026-05-28 14:45:13 +0300

    Delete api_v1 directory

--- api_v1/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Journal
-
-
-class JournalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Journal
-        fields = '__all__'
